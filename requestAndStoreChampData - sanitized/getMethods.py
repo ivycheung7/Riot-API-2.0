90d7c90a19905a8dbfc6e260d3fd01d66a188f9a
ExiliumAeternum <shuyuchen1121@gmail.com>
2015-08-19 11:54:48 -0400

    added exception in getMethod for keyErrors, loops through all matchId and gets all data

--- requestAndStoreChampData - sanitized/getMethods.py
+++ requestAndStoreChampData - sanitized/getMethods.py
@@ -2,6 +2,8 @@
 This is a module to access the match information when json is turned into a dictionary
 '''
 
+import logging #To just note down all of the matchIds and their errors, then fix it later
+
 def getChampionId(matchDataDict, index):
     '''Returns championId from dictionary as an int'''
     return matchDataDict['participants'][index]['championId']
@@ -16,8 +18,16 @@ def getWinnerPlayer(matchDataDict, index):
 
 def getBannedChampId(matchDataDict, index):
     '''Return a list of banned champion Id made by team 1 or team 2, depending on index'''
+    logging.basicConfig(filename='exceptions.log', level=logging.DEBUG) #Made a log file for all the exceptions
     bannedChampList = [] #To put champ Ids in and return it
-    teamBansList =  matchDataDict['teams'][index]['bans'] #Returns a list of 3 dictionaries, each with pick turn and banned champ
-    for bans in teamBansList:
-        bannedChampList.append(bans['championId']) #Adds the champion Id to the ban list
+    try:
+        teamBansList =  matchDataDict['teams'][index]['bans'] #Returns a list of 3 dictionaries, each with pick turn and banned champ
+        if len(teamBansList) < 3: #Logging to see if some teams only have 1 or 2 bans
+            logging.debug('This ban list is the len of '+ len(teamBansList) + ', teamId = ' +matchDataDict['matchId'])
+        for bans in teamBansList:
+            bannedChampList.append(bans['championId']) #Adds the champion Id to the ban list
+    except Exception, e: #Throws exceptions, log them, and then debug them later
+        logging.info(matchDataDict['matchId'])
+        logging.warning('teamId ' + matchDataDict['teamId'] + 'does not have a ban section')
+        logging.exception(e)
     return bannedChampList
--- requestAndStoreChampData - sanitized/requestAndStoreDataUsingJson.py
+++ requestAndStoreChampData - sanitized/requestAndStoreDataUsingJson.py
@@ -17,41 +17,56 @@ Questions to be answered with data collectd:
     Champion ban rate vs win rate
     Champion ward rate vs win rate
 '''
+from time import sleep #To pause the program when response 429 comes up
 import requests
 import json
 import getMethods #A module I wrote myself for all get methods for the dictionary
 from updateDatabase import updateDatabase #A module I wrote myself for all update database methods
 
 #Puting all sensitive information here so I can delete them easily later. They shouldn't change
-API_KEY = <API KEY HERE>
-SERVER = <SERVER HERE>
-TABLE = <TABLE NAME HERE> #Name of the table you want to update
+API_KEY = <INSERT API KEY HERE>
+SERVER = <INSERT SERVER HERE>
+TABLE = <INSERT TABLE NAME HERE> #Name of the table you want to update
 
 def requestData(matchId, SERVER, API_KEY):
     '''Sends request to API and retrieve the response. Then convert json and return a dictionary'''
-    url = 'https://na.api.pvp.net/api/lol/' + SERVER + '/v2.2/match/' + matchId + '?api_key=' + API_KEY
+    url = 'https://na.api.pvp.net/api/lol/' + SERVER + '/v2.2/match/' + str(matchId) + '?api_key=' + API_KEY
     r = requests.get(url)
+    while r.status_code != 200: #Because response 429 happens way too often
+        print r.status_code
+        sleep(5) #Wait 10 seconds and try again
+        r = requests.get(url)
     #print r #For testing purposes
     jsonDict = json.loads(r.text)
     return jsonDict
 
 def getAndStoreData(matchDataDict):
-    '''Opens database, update information, and close connection. Tables are pre-made already with necessary columns'''
+    '''
+    Opens database, update information, and close connection. Tables are pre-made already with necessary columns
+    Layout of my table:"|  id  |  name  |  title  |  picked  |  won  |  banned  |  warded  |"
+    '''
     #Finding out champion pick rate, win rate and ward rate
     for index in range (0, 10): #Because I know for a fact there are 10 players in a game
         champId = getMethods.getChampionId(matchDataDict, index) #gets championId from dictionary
         wardsPlaced = getMethods.getWardsPlaced(matchDataDict, index) #gets wardsPlaced from dictionary
-        updateDatabase(TABLE, <COLUMN NAME HERE>, champId) #Update database with +1 for picked for that champion
-        updateDatabase(TABLE, <COLUMN NAME HERE>, champId, wardsPlaced) #Update database, adding to existing number of wards
+        updateDatabase(TABLE, 'picked', champId) #Update database with +1 for picked for that champion
+        updateDatabase(TABLE, 'warded', champId, wardsPlaced) #Update database, adding to existing number of wards
         if getMethods.getWinnerPlayer(matchDataDict, index): #If True, because function returns boolean
-            updateDatabase(TABLE, <COLUMN NAME HERE>, champId)
+            updateDatabase(TABLE, 'won', champId)
     for index in range (0, 2): #Because I know there are 2 teams in this game
         bannedChampList = getMethods.getBannedChampId(matchDataDict, index)
         for bannedChampId in bannedChampList:
-            updateDatabase(TABLE, <COLUMN NAME HERE>, bannedChampId)
+            updateDatabase(TABLE, 'banned', bannedChampId)
 
 def main():
-    matchDataDict = requestData(matchId, SERVER, API_KEY)
-    getAndStoreData(matchDataDict)
-
+    #Open the file with all the match IDs
+    jsonFile = open('NA.json','r') #Make sure you have the json file with all the matchId in the same folder as this script
+    #Take all the content out of the file
+    jsonContent = jsonFile.read()
+    #Get all the matchId and put it in a list
+    allMatchId = json.loads(jsonContent)
+    for matchId in allMatchId:
+        matchDataDict = requestData(matchId, SERVER, API_KEY)
+        getAndStoreData(matchDataDict)
+    jsonFile.close()
 main()
